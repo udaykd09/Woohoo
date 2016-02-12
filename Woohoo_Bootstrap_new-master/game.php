9c25b8d29fbaca9e6de54e5ba2529c1916aaf628
Uday Dungarwal <udaykd09@users.noreply.github.com>
2016-02-11 20:24:28 -0800

    Update game.php

--- Woohoo_Bootstrap_new-master/game.php
+++ Woohoo_Bootstrap_new-master/game.php
@@ -176,7 +176,6 @@ $mail = $_SESSION ['mail'];
      var charArr = new Array('a','b','c','d','e','f','g','h','i');
     var gameFinal = new Array(0,1,2,3,4,5,6,7,8);
     var xhr = false;
-    var name = "saurabh";
     
     function initAll() {
         var i;
